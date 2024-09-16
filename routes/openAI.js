5cf1179a5c942efd76b387fce86c1bc9a9322619
piyush2001-git <piyush.sultaniya@in.ey.com>
2024-09-16 16:41:52 +0530

    refactoring

--- routes/openAI.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const openAIController = require('./../controllers/openAI');
-
-const router = express.Router();
-
-router.post('/generate-chat', openAIController.generateQuery);
-
-module.exports = router;
